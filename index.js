1df0d8665e646c3c6cc65a135672a493d40ea263
Shapel Nahyrych <shapel.nahyrych@gmail.com>
2025-08-11 09:16:58 +0100

    added while

--- index.js
+++ index.js
@@ -413,3 +413,17 @@
 // }
 // console.groupEnd();
 
+// let amountPlate = 3;
+
+// while (amountPlate>0) {
+//   console.log('washing plate' , amountPlate) ;
+//   amountPlate--;
+// }
+
+let amountPlate = 0;
+const totalAmountPlate =5;
+while (amountPlate < totalAmountPlate) {
+    amountPlate++;
+    console.log('washing plate' , amountPlate);
+}
+
